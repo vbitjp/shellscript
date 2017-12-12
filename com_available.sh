353a69b0aaaa2fffe85a985c6667990a77f68c65
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2017-12-13 02:44:51 +0900

    Changed some incorrect description

--- com_available.sh
+++ com_available.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Checker script installed commands for server monitoring
+# Checker script to find commands not-installed for server monitoring
 echo 'Available commands are as followings...'
 for command1 in 'top' 'iostat' 'mpstat' 'vmstat' 'vm_stat' 'sar' 'ps' 'uptime' 'w' 'free' 'swapon' 'netstat' 'pstree' 'lsof';do
 	Dirname=$(which $command1)
 	if echo $Dirname | grep '/'$command1 >/dev/null 2>&1; then
     	echo $command1
-    else
+	else
     	ngarray+=$command1'\n'
 	fi
 done
