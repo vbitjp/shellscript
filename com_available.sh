bf8dd9f3a1c61f8d87f09294729c18cb68219889
7ywrxsxrw7z571g@vbit.jp <7ywrxsxrw7z571g@vbit.jp>
2017-12-13 02:46:11 +0900

    Changed some incorrect description

--- com_available.sh
+++ com_available.sh
@@ -4,9 +4,9 @@ echo 'Available commands are as followings...'
 for command1 in 'top' 'iostat' 'mpstat' 'vmstat' 'vm_stat' 'sar' 'ps' 'uptime' 'w' 'free' 'swapon' 'netstat' 'pstree' 'lsof';do
 	Dirname=$(which $command1)
 	if echo $Dirname | grep '/'$command1 >/dev/null 2>&1; then
-    	echo $command1
+		echo $command1
 	else
-    	ngarray+=$command1'\n'
+		ngarray+=$command1'\n'
 	fi
 done
 echo '\nNot-installed commands are as followings...'
